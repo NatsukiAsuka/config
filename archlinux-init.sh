2b00c5925c5f8200ccc9d9ebd91ccc74137d2275
NatsukiAsuka <NatsukiAsuka1973@gmail.com>
2018-07-03 20:25:38 +0800

    update

--- archlinux-init.sh
+++ archlinux-init.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-sudo pacman -Syu atom  visual-studio-code-bin  nodejs npm git google-chrome  firefox  zsh fish telegram-desktop  webstorm  intellij-idea-ultimate-edition  goland scala go jdk8-openjdk  jdk9-openjdk jdk10-openjdk maven make cmake stack clang kotlin  clojure  vim  yakuake  neofetch htop ipython uget  qbittorrent  emacs imagewriter  okular typora  fcitx-sogoupinyin  fcitx-configtool  fcitx mariadb  postman-bin  gimp vlc  besttrace gufw  filezilla nmap
+sudo pacman -Syu atom  visual-studio-code-bin  nodejs npm git google-chrome  firefox  zsh fish telegram-desktop  webstorm  intellij-idea-ultimate-edition  goland scala go jdk8-openjdk  jdk9-openjdk jdk10-openjdk maven make cmake stack clang kotlin  clojure  vim  yakuake  neofetch htop ipython uget  qbittorrent  emacs imagewriter  okular typora  fcitx-sogoupinyin  fcitx-configtool  fcitx mariadb  postman-bin  gimp vlc  besttrace gufw  filezilla nmap  tmux ack powertop iotop iptop curl  docker slack thefuck cloc tldr jq &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
+curl -sLf https://spacevim.org/install.sh | bash 
